641901211216d55aa562605ef16b0745802d6a37
Jonatan Antoni <jonatan.antoni@arm.com>
2023-04-19 12:34:35 +0200

    Add link checker

--- DoxyGen/gen_doc.sh
+++ DoxyGen/gen_doc.sh
@@ -1,21 +1,65 @@
 #!/bin/bash
-# Version: 2.0
-# Date: 2023-03-31
+# Version: 2.1
+# Date: 2023-04-19
 # This bash script generates AVH Documentation:
 #
 # Pre-requisites:
 # - bash shell (for Windows: install git for Windows)
-# - doxygen 
-# - graphviz
+# - doxygen 1.9.6
+# - graphviz (dot)
 # - plantuml
 
 set -o pipefail
 
+# Set version of gen pack library
+REQUIRED_GEN_PACK_LIB="0.7.0"
+
 DIRNAME=$(dirname $(readlink -f $0))
 DOXYGEN=$(which doxygen 2>/dev/null)
+
 REQ_DXY_VERSION="1.9.6"
-GVDOT=$(which dot 2>/dev/null)
-PLANTUML=$(which plantuml 2>/dev/null)
+
+############ DO NOT EDIT BELOW ###########
+
+function install_lib() {
+  local URL="https://github.com/Open-CMSIS-Pack/gen-pack/archive/refs/tags/v$1.tar.gz"
+  echo "Downloading gen-pack lib to '$2'"
+  mkdir -p "$2"
+  curl -L "${URL}" -s | tar -xzf - --strip-components 1 -C "$2" || exit 1
+}
+
+function load_lib() {
+  if [[ -d ${GEN_PACK_LIB} ]]; then
+    . "${GEN_PACK_LIB}/gen-pack"
+    return 0
+  fi
+  local GLOBAL_LIB="/usr/local/share/gen-pack/${REQUIRED_GEN_PACK_LIB}"
+  local USER_LIB="${HOME}/.local/share/gen-pack/${REQUIRED_GEN_PACK_LIB}"
+  if [[ ! -d "${GLOBAL_LIB}" && ! -d "${USER_LIB}" ]]; then
+    echo "Required gen-pack lib not found!" >&2
+    install_lib "${REQUIRED_GEN_PACK_LIB}" "${USER_LIB}"
+  fi
+
+  if [[ -d "${GLOBAL_LIB}" ]]; then
+    . "${GLOBAL_LIB}/gen-pack"
+  elif [[ -d "${USER_LIB}" ]]; then
+    . "${USER_LIB}/gen-pack"
+  else
+    echo "Required gen-pack lib is not installed!" >&2
+    exit 1
+  fi
+}
+
+load_lib
+
+find_doxygen "${REQ_DXY_VERSION}"
+GVDOT="$(find_utility dot)"
+report_utility "dot" "$GVDOT" $?
+PLANTUML="$(find_utility plantuml)"
+report_utility "plantuml" "$PLANTUML" $?
+
+###############################################
+
 REGEN=0
 ALLPARTS=($(find ${DIRNAME} -mindepth 1 -maxdepth 1 -type d -exec basename {} \;))
 PARTS=()
@@ -30,50 +74,19 @@ else
     done
 fi
 
-if [[ ! -f "${DOXYGEN}" ]]; then
-    echo "Doxygen not found!" >&2
-    echo "Did you miss to add it to PATH?"
-    exit 1
-else
-    version=$("${DOXYGEN}" --version | sed -E 's/.*([0-9]+\.[0-9]+\.[0-9]+).*/\1/')
-    echo "Doxygen is ${DOXYGEN} at version ${version}"
-    if [[ "${version}" != "${REQ_DXY_VERSION}" ]]; then
-        echo "Doxygen is required to be at version ${REQ_DXY_VERSION}!" >&2
-        exit 1
-    fi
-fi
-
-if [[ ! -f "${GVDOT}" ]]; then
-    echo "Graphviz/dot not found!" >&2
-    echo "Did you miss to add it to PATH?"
-    exit 1
-else
-    version=$("${GVDOT}" -V 2>&1 | sed -E 's/.*version ([0-9]+\.[0-9]+\.[0-9]+).*/\1/')
-    echo "Graphviz/dot is ${GVDOT} at version ${version}"
-fi
-
-if [[ ! -f "${PLANTUML}" ]]; then
-    echo "PlantUML not found!" >&2
-    echo "Did you miss to add it to PATH?"
-    exit 1
-else
-    version=$("${PLANTUML}" -version | grep -E 'PlantUML version' | sed -E 's/.*version ([0-9]+\.[0-9]+\.[0-9]+).*/\1/')
-    echo "PlantUML is ${PLANTUML} at version ${version}"
-fi
-
 function doxygen {
     partname=$(basename $(dirname $1))
     if [[ $REGEN != 0 ]] || [[ " ${PARTS[@]} " =~ " ${partname} " ]]; then
         pushd "$(dirname $1)" > /dev/null
-        echo "${DOXYGEN} $1"
-        "${DOXYGEN}" $(basename "$1")
+        echo "${UTILITY_DOXYGEN} $1"
+        "${UTILITY_DOXYGEN}" $(basename "$1")
         popd > /dev/null
-        
+
         if [[ $2 != 0 ]]; then
         #    mkdir -p "${DIRNAME}/../Documentation/${partname}/html/search/"
             cp -f "${DIRNAME}/Doxygen_Templates/navtree.js" "${DIRNAME}/../Documentation/${partname}/html/"
         fi
-        
+
         projectName=$(grep -E "PROJECT_NAME\s+=" $1 | sed -r -e 's/[^"]*"([^"]+)".*/\1/')
         projectNumber=$(grep -E "PROJECT_NUMBER\s+=" $1 | sed -r -e 's/[^"]*"([^"]+)".*/\1/')
         datetime=$(date -u +'%a %b %e %Y %H:%M:%S')
