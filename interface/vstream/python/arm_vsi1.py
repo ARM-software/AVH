6e792143894ca8fd6273f02159abb7058584246d
Vladimir Umek <vladimir.umek@arm.com>
2025-07-24 10:52:17 +0200

    Add vStream AudioIn/AudioOut implementations using VSI

---
+++ interface/vstream/python/arm_vsi1.py
@@ -0,0 +1,332 @@
+# Copyright (c) 2021-2025 Arm Limited. All rights reserved.
+
+# Virtual Streaming Interface instance 1 Python script: Audio Output
+
+##@addtogroup arm_vsi1_py_audio_out
+#  @{
+#
+##@package arm_vsi1_audio_out
+#Documentation for VSI Audio Output module.
+#
+#More details.
+
+import logging
+import wave
+import os
+
+logger = logging.getLogger(__name__)
+
+## Set verbosity level
+#verbosity = logging.DEBUG
+#verbosity = logging.INFO
+#verbosity = logging.WARNING
+verbosity = logging.ERROR
+
+# [debugging] Verbosity settings
+level = { 10: "DEBUG",  20: "INFO",  30: "WARNING",  40: "ERROR" }
+logging.basicConfig(format='Py: %(name)s : [%(levelname)s]\t%(message)s', level = verbosity)
+logger.info("Verbosity level is set to " + level[verbosity])
+
+
+# IRQ registers
+IRQ_Status = 0
+
+# Timer registers
+Timer_Control  = 0
+Timer_Interval = 0
+
+# Timer Control register definitions
+Timer_Control_Run_Msk      = 1<<0
+Timer_Control_Periodic_Msk = 1<<1
+Timer_Control_Trig_IRQ_Msk = 1<<2
+Timer_Control_Trig_DMA_Msk = 1<<3
+
+# DMA registers
+DMA_Control = 0
+
+# DMA Control register definitions
+DMA_Control_Enable_Msk    = 1<<0
+DMA_Control_Direction_Msk = 1<<1
+DMA_Control_Direction_P2M = 0<<1
+DMA_Control_Direction_M2P = 1<<1
+
+# User registers
+Regs = [0] * 64
+
+STATUS      = 0  # Regs[0]
+CONTROL     = 0  # Regs[1]
+CHANNELS    = 0  # Regs[2]
+SAMPLE_BITS = 0  # Regs[3]
+SAMPLE_RATE = 0  # Regs[4]
+
+# STATUS register definitions
+STATUS_OPEN_Msk = 1<<0 # Stream Open
+STATUS_DATA_Msk = 1<<1 # Data Available
+STATUS_EOS_Msk  = 1<<2 # End of Stream
+
+# User CONTROL register definitions
+CONTROL_ENABLE_Msk = 1<<0 #Enable Streaming
+
+# Data buffer
+Data = bytearray()
+
+# Global WAVE object
+WAVE = None
+
+## Open WAVE file (store object into global WAVE object)
+#  @param name name of WAVE file to open
+#  @return True if successfully opened, False otherwise
+def openWAVE(name):
+    global WAVE
+    abs_path = os.path.abspath(name)
+    logger.info("Open WAVE file: {}".format(abs_path))
+
+    try:
+        WAVE = wave.open(abs_path, 'wb')
+
+        # Set WAV file properties
+        WAVE.setnchannels(CHANNELS)
+        WAVE.setsampwidth((SAMPLE_BITS + 7) // 8)
+        WAVE.setframerate(SAMPLE_RATE)
+
+        # Output WAVE file properties
+        logger.info("  Number of channels: {}".format(CHANNELS))
+        logger.info("  Sample bits: {}".format(SAMPLE_BITS))
+        logger.info("  Sample rate: {}".format(SAMPLE_RATE))
+        return True
+    except Exception as e:
+        logger.error("Error opening WAVE file {}: {}".format(abs_path, str(e)))
+        WAVE = None
+        return False
+
+## Write WAVE frames (global WAVE object)
+#  @param frames frames to write
+def writeWAVE(frames):
+    global WAVE
+    logger.info("Write WAVE frames")
+    if WAVE is None:
+        logger.error("No valid WAVE file is open")
+    try:
+        WAVE.writeframes(frames)
+    except Exception as e:
+        logger.error("Error writing WAVE frames: {}".format(str(e)))
+
+## Close WAVE file (global WAVE object)
+def closeWAVE():
+    global WAVE
+    logger.info("Close WAVE file")
+    if WAVE is not None:
+        try:
+            WAVE.close()
+
+        except Exception as e:
+            logger.error("Error closing WAVE file: {}".format(str(e)))
+        finally:
+            WAVE = None
+    else:
+        logger.warning("No WAVE file to close")
+
+
+## Write a block of data from the global Data buffer
+#  @param block_size size of block to write (in bytes)
+def writeDataBlock(block_size):
+    global Data
+    logger.info("Write block of data from the data buffer")
+
+    writeWAVE(Data)
+    # Set DATA bit after a block of data is written
+    STATUS |= STATUS_DATA_Msk
+    logger.debug("STATUS register updated: DATA bit set")
+
+
+## Initialize
+def init():
+    logger.info("Python function init() called")
+
+
+## Read interrupt request (the VSI IRQ Status Register)
+#  @return value value read (32-bit)
+def rdIRQ():
+    global IRQ_Status
+    logger.info("Python function rdIRQ() called")
+
+    value = IRQ_Status
+    logger.debug("Read interrupt request: {}".format(value))
+
+    return value
+
+
+## Write interrupt request (the VSI IRQ Status Register)
+#  @param value value to write (32-bit)
+#  @return value value written (32-bit)
+def wrIRQ(value):
+    global IRQ_Status
+    logger.info("Python function wrIRQ() called")
+
+    IRQ_Status = value
+    logger.debug("Write interrupt request: {}".format(value))
+
+    return value
+
+
+## Write Timer registers (the VSI Timer Registers)
+#  @param index Timer register index (zero based)
+#  @param value value to write (32-bit)
+#  @return value value written (32-bit)
+def wrTimer(index, value):
+    global Timer_Control, Timer_Interval
+    logger.info("Python function wrTimer() called")
+
+    if   index == 0:
+        Timer_Control = value
+        logger.debug("Write Timer_Control: {}".format(value))
+    elif index == 1:
+        Timer_Interval = value
+        logger.debug("Write Timer_Interval: {}".format(value))
+
+    return value
+
+
+## Timer event (called at Timer Overflow)
+def timerEvent():
+    logger.info("Python function timerEvent() called")
+
+
+## Write DMA registers (the VSI DMA Registers)
+#  @param index DMA register index (zero based)
+#  @param value value to write (32-bit)
+#  @return value value written (32-bit)
+def wrDMA(index, value):
+    global DMA_Control
+    logger.info("Python function wrDMA() called")
+
+    if   index == 0:
+        DMA_Control = value
+        logger.debug("Write DMA_Control: {}".format(value))
+
+    return value
+
+
+## Read data from peripheral for DMA P2M transfer (VSI DMA)
+#  @param size size of data to read (in bytes, multiple of 4)
+#  @return data data read (bytearray)
+def rdDataDMA(size):
+    global Data
+    logger.info("Python function rdDataDMA() called")
+
+    n = min(len(Data), size)
+    data = bytearray(size)
+    data[0:n] = Data[0:n]
+    logger.debug("Read data ({} bytes)".format(size))
+
+    return data
+
+
+## Write data to peripheral for DMA M2P transfer (VSI DMA)
+#  @param data data to write (bytearray)
+#  @param size size of data to write (in bytes, multiple of 4)
+def wrDataDMA(data, size):
+    global Data
+    logger.info("Python function wrDataDMA() called")
+
+    Data = data
+    logger.debug("Write data ({} bytes)".format(size))
+
+    writeDataBlock(size)
+
+    return
+
+
+## Write CONTROL register (user register)
+#  @param value value to write (32-bit)
+def wrCONTROL(value):
+    global CONTROL, STATUS
+    if ((value ^ CONTROL) & CONTROL_ENABLE_Msk) != 0:
+        if (value & CONTROL_ENABLE_Msk) != 0:
+            logger.info("CONTROL register updated: ENABLE bit set")
+            if openWAVE('test.wav'):
+                # File opened successfully, stream is open
+                STATUS |= STATUS_OPEN_Msk
+                logger.debug("STATUS register updated: OPEN bit set")
+        else:
+            logger.info("CONTROL register updated: ENABLE bit cleared")
+            closeWAVE()
+            # Clear OPEN bit when stream is closed
+            STATUS &= ~STATUS_OPEN_Msk
+            logger.debug("STATUS register updated: OPEN bit cleared")
+    CONTROL = value
+
+## Write CHANNELS register (user register)
+#  @param value value to write (32-bit)
+def wrCHANNELS(value):
+    global CHANNELS
+    CHANNELS = value
+    logger.info("CHANNELS: {}".format(value))
+
+## Write SAMPLE_BITS register (user register)
+#  @param value value to write (32-bit)
+def wrSAMPLE_BITS(value):
+    global SAMPLE_BITS
+    SAMPLE_BITS = value
+    logger.info("SAMPLE_BITS: {}".format(value))
+
+## Write SAMPLE_RATE register (user register)
+#  @param value value to write (32-bit)
+def wrSAMPLE_RATE(value):
+    global SAMPLE_RATE
+    SAMPLE_RATE = value
+    logger.info("SAMPLE_RATE: {}".format(value))
+
+## Read STATUS register (user register)
+#  @return value value read (32-bit)
+def rdSTATUS():
+    global STATUS
+    logger.info("STATUS: {}".format(STATUS))
+    value = STATUS
+    # Clear DATA bit on read of STATUS register
+    STATUS &= ~STATUS_DATA_Msk
+    logger.debug("STATUS register updated: DATA bit cleared")
+    return value
+
+## Read user registers (the VSI User Registers)
+#  @param index user register index (zero based)
+#  @return value value read (32-bit)
+def rdRegs(index):
+    global Regs
+    logger.info("Python function rdRegs() called")
+
+    if index == 0:
+        value = rdSTATUS()
+    else:
+        value = Regs[index]
+
+    logger.debug("Read user register at index {}: {}".format(index, value))
+
+    return value
+
+
+## Write user registers (the VSI User Registers)
+#  @param index user register index (zero based)
+#  @param value value to write (32-bit)
+#  @return value value written (32-bit)
+def wrRegs(index, value):
+    global Regs
+    logger.info("Python function wrRegs() called")
+
+    if   index == 1:
+        wrCONTROL(value)
+    elif index == 2:
+        wrCHANNELS(value)
+    elif index == 3:
+        wrSAMPLE_BITS(value)
+    elif index == 4:
+        wrSAMPLE_RATE(value)
+
+    Regs[index] = value
+    logger.debug("Write user register at index {}: {}".format(index, value))
+
+    return value
+
+
+## @}
+
---
+++ interface/vstream/source/vstream_audio_out.c
@@ -0,0 +1,424 @@
+/*---------------------------------------------------------------------------
+ * Copyright (c) 2025 Arm Limited (or its affiliates).
+ * All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the License); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an AS IS BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *---------------------------------------------------------------------------*/
+
+#include <stdint.h>
+#include <string.h>
+#include "cmsis_vstream.h"
+
+#ifdef _RTE_
+#include "RTE_Components.h"
+#endif
+#include CMSIS_device_header
+#include "arm_vsi.h"
+
+/* Number of channels 1=Mono, 2=Stereo            */
+/* Defines the number of audio channels in stream */
+#ifndef AUDIO_OUT_CHANNELS
+#define AUDIO_OUT_CHANNELS     2U
+#endif
+
+/* Number of bits per sample <0=>8 <1=>16 <2=>24 <3=>32  */
+/* Defines number of bits of information in each sample. */
+#ifndef AUDIO_OUT_SAMPLE_BITS
+#define AUDIO_OUT_SAMPLE_BITS  16U
+#endif
+
+/* Sample rate (samples per second)                   */
+/* Defines the number of samples captured per second. */
+#ifndef AUDIO_OUT_SAMPLE_RATE
+#define AUDIO_OUT_SAMPLE_RATE  16000U
+#endif
+
+
+/* Audio Peripheral definitions */
+#define AudioOut              ARM_VSI1                /* Audio Output access struct     */
+#define AudioOut_IRQn         ARM_VSI1_IRQn           /* Audio Output Interrupt number  */
+#define AudioOut_Handler      ARM_VSI1_Handler        /* Audio Output Interrupt handler */
+
+/* Audio Peripheral registers */
+#define STATUS                Regs[0]                 /* Status register           */
+#define CONTROL               Regs[1]                 /* Control receiver          */
+#define CHANNELS              Regs[2]                 /* Number of channels        */
+#define SAMPLE_BITS           Regs[3]                 /* Number of bits per sample */
+#define SAMPLE_RATE           Regs[4]                 /* Samples per second        */
+
+/* STATUS register definitions */
+#define STATUS_OPEN_Msk       (1UL << 0)              /* Stream Open    */
+#define STATUS_DATA_Msk       (1UL << 1)              /* Data Available */
+#define STATUS_EOS_Msk        (1UL << 2)              /* End of Stream  */
+
+/* Audio Control register definitions */
+#define CONTROL_ENABLE_Msk    (1UL << 0)              /* CONTROL: ENABLE Streaming */
+
+/* Handle Flags Definitions */
+#define FLAGS_INIT            (1U << 0)
+#define FLAGS_START           (1U << 1)
+#define FLAGS_SINGLE          (1U << 2)
+#define FLAGS_LIMIT_OWN       (1U << 3)
+#define FLAGS_BUF_EMPTY       (1U << 4)
+#define FLAGS_BUF_FULL        (1U << 5)
+
+/* Stream Buffer Type Definition */
+typedef struct stream_buf_s {
+  uint8_t *data;              /* Data buffer pointer             */
+  uint32_t block_num;         /* Number of blocks in data buffer */
+  uint32_t block_size;        /* Size of block in data buffer    */
+} StreamBuf_t;
+
+/* vStream Handle Type Definition */
+typedef struct {
+  vStreamEvent_t    callback; /* Callback from vStream driver */
+  StreamBuf_t       buf;      /* Stream buffer    */
+  volatile uint32_t idx_get;  /* Index of block to be returned on GetBlock call     */
+  volatile uint32_t idx_rel;  /* Index of block to be released on ReleaseBlock call */
+  volatile uint32_t idx_out;  /* Index of block currently being streamed            */
+  volatile uint8_t  active;   /* Streaming active flag  */
+  volatile uint8_t  underflow; /* Buffer underflow flag */
+  volatile uint8_t  eos;      /* End of stream flag     */
+  volatile uint8_t  flags;    /* Handle status flags    */
+} StreamHandle_t;
+
+/* vStream Handle */
+static StreamHandle_t hAudioOut = {0};
+
+/**
+  \brief Audio Output Interrupt Handler.
+*/
+void AudioOut_Handler (void) {
+  uint32_t status;
+  uint32_t event;
+
+  status = AudioOut->STATUS;
+
+  AudioOut->IRQ.Clear = 0x00000001U;
+  __DSB();
+  __ISB();
+
+  event = 0U;
+
+  if ((status & STATUS_DATA_Msk) != 0U) {
+    /* Data block streamed out */
+    event |= VSTREAM_EVENT_DATA;
+
+    /* Increment index of the block being streamed */
+    hAudioOut.idx_out = (hAudioOut.idx_out + 1) % hAudioOut.buf.block_num;
+
+    /* Clear buffer full flag */
+    hAudioOut.flags &= ~FLAGS_BUF_FULL;
+
+    if (hAudioOut.idx_out == hAudioOut.idx_rel) {
+      /* Buffer is empty */
+      hAudioOut.flags |= FLAGS_BUF_EMPTY;
+
+      if ((hAudioOut.flags & FLAGS_SINGLE) == 0U) {
+        /* Continuous mode, no new stream buffer */
+        hAudioOut.underflow = 1U;
+
+        event |= VSTREAM_EVENT_UNDERFLOW;
+      }
+    }
+  }
+
+  if ((status & STATUS_EOS_Msk) != 0U) {
+    /* End of stream */
+    hAudioOut.eos = 1U;
+    event |= VSTREAM_EVENT_EOS;
+  }
+
+  if (hAudioOut.flags & FLAGS_SINGLE) {
+    /* Single mode, clear active flag */
+    hAudioOut.active = 0U;
+  }
+
+  if ((hAudioOut.callback != NULL) && (event != 0U)) {
+    /* Call application callback function */
+    hAudioOut.callback(event);
+  }
+}
+
+/* Initialize streaming interface */
+static int32_t Initialize (vStreamEvent_t event_cb) {
+
+  hAudioOut.callback  = event_cb;
+  hAudioOut.active    = 0U;
+  hAudioOut.underflow = 0U;
+  hAudioOut.eos       = 0U;
+  hAudioOut.flags     = 0U;
+
+  /* Initialize Audio Input peripheral */
+  AudioOut->Timer.Control = 0U;
+  AudioOut->DMA.Control   = 0U;
+  AudioOut->IRQ.Clear     = 0x00000001U;
+  AudioOut->IRQ.Enable    = 0x00000001U;
+  AudioOut->CONTROL       = 0U;
+
+  /* Set audio configuration */
+  AudioOut->CHANNELS    = AUDIO_OUT_CHANNELS;
+  AudioOut->SAMPLE_BITS = AUDIO_OUT_SAMPLE_BITS;
+  AudioOut->SAMPLE_RATE = AUDIO_OUT_SAMPLE_RATE;
+
+  /* Enable peripheral interrupts */
+//NVIC_EnableIRQ(AudioOut_IRQn);
+  NVIC->ISER[(((uint32_t)AudioOut_IRQn) >> 5UL)] = (uint32_t)(1UL << (((uint32_t)AudioOut_IRQn) & 0x1FUL));
+  __DSB();
+  __ISB();
+
+  /* Driver is initialized */
+  hAudioOut.flags |= FLAGS_INIT;
+
+  return (VSTREAM_OK);
+}
+
+/* De-initialize streaming interface */
+static int32_t Uninitialize (void) {
+
+  /* Disable peripheral interrupts */
+//NVIC_DisableIRQ(AudioOut_IRQn);
+  NVIC->ICER[(((uint32_t)AudioOut_IRQn) >> 5UL)] = (uint32_t)(1UL << (((uint32_t)AudioOut_IRQn) & 0x1FUL));
+  __DSB();
+  __ISB();
+
+  /* De-initialize Audio Input peripheral */
+  AudioOut->Timer.Control = 0U;
+  AudioOut->DMA.Control   = 0U;
+  AudioOut->IRQ.Clear     = 0x00000001U;
+  AudioOut->IRQ.Enable    = 0x00000000U;
+  AudioOut->CONTROL       = 0U;
+
+  /* Clear audio control block structure */
+  memset(&hAudioOut, 0, sizeof(hAudioOut));
+
+  return VSTREAM_OK;
+
+}
+/* Set streaming data buffer */
+static int32_t SetBuf (void *buf, uint32_t buf_size, uint32_t block_size) {
+  int32_t status;
+
+  if (buf == NULL) {
+    status = VSTREAM_ERROR_PARAMETER;
+  }
+  else if ((buf_size == 0U) || (block_size == 0U) || (block_size > buf_size)) {
+    status = VSTREAM_ERROR_PARAMETER;
+  }
+  else if ((hAudioOut.flags & FLAGS_INIT) == 0) {
+    /* Not initialized */
+    status = VSTREAM_ERROR;
+  }
+  else if (hAudioOut.active == 1U) {
+    /* Streaming is active */
+    status = VSTREAM_ERROR;
+  }
+  else {
+    hAudioOut.buf.data       = (uint8_t *)buf;
+    hAudioOut.buf.block_num  = buf_size / block_size;
+    hAudioOut.buf.block_size = block_size;
+
+    /* Buffer is empty */
+    hAudioOut.flags |= FLAGS_BUF_EMPTY;
+
+    /* Reset indexes */
+    hAudioOut.idx_out = 0U;
+    hAudioOut.idx_get = 0U;
+    hAudioOut.idx_rel = 0U;
+
+    AudioOut->DMA.Address   = (uint32_t)buf;
+    AudioOut->DMA.BlockNum  = buf_size / block_size;
+    AudioOut->DMA.BlockSize = block_size;
+
+    status = VSTREAM_OK;
+  }
+
+  return (status);
+}
+/* Start streaming */
+static int32_t Start (uint32_t mode) {
+  int32_t  rval;
+  uint32_t ctrl;
+  uint32_t sample_size;
+  uint32_t sample_rate;
+  uint32_t block_size;
+
+  if ((hAudioOut.flags & FLAGS_INIT) == 0) {
+    /* Not initialized */
+    rval = VSTREAM_ERROR;
+  }
+  else if (hAudioOut.buf.data == NULL) {
+    /* Buffer not set */
+    rval = VSTREAM_ERROR;
+  }
+  else if ((hAudioOut.flags & FLAGS_BUF_EMPTY) != 0) {
+    /* Buffer is empty */
+    rval = VSTREAM_ERROR;
+  }
+  else if (hAudioOut.active == 1U) {
+    /* Already active */
+    rval = VSTREAM_OK;
+  }
+  else {
+    rval = VSTREAM_OK;
+
+    /* Set active status */
+    hAudioOut.active = 1U;
+
+    if (mode == VSTREAM_MODE_SINGLE) {
+      /* Single mode */
+      hAudioOut.flags |= FLAGS_SINGLE;
+
+      /* Initialize timer control register for one-shot mode */
+      ctrl = ARM_VSI_Timer_Trig_DMA_Msk | ARM_VSI_Timer_Trig_IRQ_Msk;
+    }
+    else {
+      /* Continuous mode */
+      hAudioOut.flags &= ~FLAGS_SINGLE;
+
+      /* Initialize timer control register for continuous mode */
+      ctrl = ARM_VSI_Timer_Trig_DMA_Msk | ARM_VSI_Timer_Trig_IRQ_Msk | ARM_VSI_Timer_Periodic_Msk;
+    }
+
+    AudioOut->CONTROL     = CONTROL_ENABLE_Msk;
+    AudioOut->DMA.Control = ARM_VSI_DMA_Direction_M2P | ARM_VSI_DMA_Enable_Msk;
+
+    sample_size = AudioOut->CHANNELS * ((AudioOut->SAMPLE_BITS + 7U) / 8U);
+    sample_rate = AudioOut->SAMPLE_RATE;
+    block_size  = AudioOut->DMA.BlockSize;
+
+    AudioOut->Timer.Interval = ((uint64_t)1000000U * (block_size / sample_size)) / sample_rate;
+
+    /* Apply configuration and start the timer */
+    AudioOut->Timer.Control = ctrl | ARM_VSI_Timer_Run_Msk;
+  }
+  return rval;
+}
+
+/* Stop streaming */
+static int32_t Stop (void) {
+  int32_t rval;
+
+  if ((hAudioOut.flags & FLAGS_INIT) == 0) {
+    /* Not initialized */
+    rval = VSTREAM_ERROR;
+  }
+  else if (hAudioOut.active == 0U) {
+    /* Not active */
+    rval = VSTREAM_OK;
+  }
+  else {
+    /* Stop the stream */
+    AudioOut->Timer.Control = 0U;
+    AudioOut->DMA.Control   = 0U;
+    AudioOut->CONTROL       = 0U;
+
+    hAudioOut.active = 0U;
+
+    rval = VSTREAM_OK;
+  }
+
+  return rval;
+}
+
+/* Get pointer to a data block */
+static void *GetBlock (void) {
+  uint32_t buf_index;
+  void *p;
+
+  if (hAudioOut.buf.data == NULL) {
+    /* Buffer not set */
+    p = NULL;
+  }
+  else if ((hAudioOut.flags & FLAGS_LIMIT_OWN) != 0) {
+    /* App already owns all the blocks */
+    p = NULL;
+  }
+  else {
+    /* Determine buffer index */
+    buf_index = hAudioOut.idx_get * hAudioOut.buf.block_size;
+
+    /* Set return pointer */
+    p = &hAudioOut.buf.data[buf_index];
+
+    /* Increment index of block returned by Get */
+    hAudioOut.idx_get = (hAudioOut.idx_get + 1U) % hAudioOut.buf.block_num;
+
+    if (hAudioOut.idx_get == hAudioOut.idx_rel) {
+      /* App owns all the blocks, set the limit flag */
+      hAudioOut.flags |= FLAGS_LIMIT_OWN;
+    }
+  }
+
+  return p;
+}
+
+/* Release data block */
+static int32_t ReleaseBlock (void) {
+  int32_t rval;
+
+  if (hAudioOut.buf.data == NULL) {
+    /* Buffer not set */
+    rval = VSTREAM_ERROR;
+  }
+  else if ((hAudioOut.idx_rel == hAudioOut.idx_get) && ((hAudioOut.flags & FLAGS_LIMIT_OWN) == 0)) {
+    /* No blocks to release */
+    rval = VSTREAM_ERROR;
+  }
+  else {
+    /* Increment index of the block to be released next */
+    hAudioOut.idx_rel = (hAudioOut.idx_rel + 1U) % hAudioOut.buf.block_num;
+
+    if (hAudioOut.idx_rel == hAudioOut.idx_out) {
+      /* Buffer is full */
+      hAudioOut.flags |= FLAGS_BUF_FULL;
+    }
+
+    /* Clear the limit get flag */
+    hAudioOut.flags &= ~FLAGS_LIMIT_OWN;
+    /* Clear the buffer empty flag */
+    hAudioOut.flags &= ~FLAGS_BUF_EMPTY;
+
+    rval = VSTREAM_OK;
+  }
+
+  return rval;
+}
+
+/* Get vStream Interface status */
+static vStreamStatus_t GetStatus (void) {
+  vStreamStatus_t status;
+
+  /* Get status */
+  status.active    = hAudioOut.active;
+  status.underflow = hAudioOut.underflow;
+  status.eos       = hAudioOut.eos;
+
+  /* Clear status */
+  hAudioOut.underflow = 0U;
+  hAudioOut.eos       = 0U;
+
+  return (status);
+}
+
+vStreamDriver_t Driver_vStreamAudioOut = {
+  Initialize,
+  Uninitialize,
+  SetBuf,
+  Start,
+  Stop,
+  GetBlock,
+  ReleaseBlock,
+  GetStatus
+};
