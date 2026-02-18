b23d3283b492e458c585713b94d38852b45e5f85
Vladimir Umek <vladimir.umek@arm.com>
2026-02-18 17:23:01 +0100

    example: Simplify FVP_Audio example
    - Move configuration separately (into app_config.h)
    - Move auxiliary definitions to accompanying header (app_main.h)
    - Remove thread_stdin and use define to set streaming duration
    - Add custom ASSERT to check return values
    - Move stream initialization to init_streams function

--- example/FVP_Audio/README.md
+++ example/FVP_Audio/README.md
@@ -3,7 +3,8 @@
 The **FVP Audio** project demonstrates the CMSIS-Driver vStream API for handling
 audio input and output streams.
 
-It shows how to capture audio samples from an input stream and output them through a audio output stream.
+It shows how to capture audio samples from an input stream and output them through
+an audio output stream.
 
 ## Operation
 
@@ -11,16 +12,24 @@ The application initializes both audio input and output streams and then continu
 captures audio samples from the input stream and copies them directly to the output
 stream.
 
+Streaming stops after receiving the `END_OF_STREAM` event or can be configured to
+stop after `AUDIO_STREAM_DURATION` seconds.
+
 The example uses CMSIS-RTOS2 for thread management and event-driven processing.
 
 ### Configuration
 
-Default configuration expects the following input and output stream format:
+Default configuration ([app_config.h](project/app_config.h)) expects the following input
+and output stream format:
 
 - 16kHz, 16-bit, 2-channels (stereo)
 
-To change default configuration use configuration defines:
+To change the default configuration use the following defines:
 
 - `AUDIO_CHANNELS`    - sets the number of channels in a stream
 - `AUDIO_SAMPLE_BITS` - sets the number of bits per sample
 - `AUDIO_SAMPLE_RATE` - sets the number of samples per second
+
+The default streaming duration is indefinite. To change this, use:
+
+- `AUDIO_STREAM_DURATION` - sets the streaming duration in seconds (0: stream indefinitely)
---
+++ example/FVP_Audio/project/app_main.h
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2026 Arm Limited and/or its affiliates.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the License); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an AS IS BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef APP_MAIN_H_
+#define APP_MAIN_H_
+
+#include <assert.h>
+#include <stdlib.h>
+
+#include "vstream_audio_in.h"
+#include "vstream_audio_out.h"
+
+#include "app_config.h"
+
+/* References to the underlying CMSIS vStream driver */
+#define vStream_AudioIn         (&Driver_vStreamAudioIn)
+#define vStream_AudioOut        (&Driver_vStreamAudioOut)
+
+/* The size of one data block that holds 100ms of audio samples */
+#define AUDIO_BUF_BLOCK_SIZE    (AUDIO_SAMPLE_RATE * (AUDIO_SAMPLE_BITS/8) * AUDIO_CHANNELS / 10)
+
+/* The number of blocks in audio buffer */
+#define AUDIO_BUF_BLOCK_CNT     (2U)
+
+/* Determine how many blocks to transfer for the required streaming duration */
+#define AUDIO_TOTAL_BLOCKS      ((AUDIO_STREAM_DURATION * AUDIO_SAMPLE_RATE) / (AUDIO_BUF_BLOCK_SIZE / (AUDIO_SAMPLE_BITS/8) / AUDIO_CHANNELS))
+
+/* Custom assert that uses stdout instead of stderr */
+#ifndef ASSERT
+#define ASSERT(expr)            do { \
+                                  if (!(expr)) { \
+                                    printf("ASSERT FAILED: %s, %s, line %d\n", #expr, __FILE__, __LINE__); \
+                                    exit(0); \
+                                  } \
+                                } while (0)
+#endif
+
+
+#endif /* APP_MAIN_H_ */
